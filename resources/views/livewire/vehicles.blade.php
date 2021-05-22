843a14889fb75f327820c87495aefbe7455d8c14
Tiago Matos <tiagovolta@gmail.com>
2021-05-22 11:16:17 -0300

    feat: created vehicles page and livewire component

---
+++ resources/views/livewire/vehicles.blade.php
@@ -0,0 +1,16 @@
+<div>
+    <div class="grid grid-cols-3 gap-4">
+        <div class="col-span-1 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">    
+            <div class="flex">
+                <div class="flex-1">
+                    <button><i class="la la-play text-lg"></i></button>
+                </div>
+                <div class="text-right flex-1">
+                    <button><i class="la la-edit text-lg"></i></button>
+                    <button><i class="la la-trash text-lg"></i></button>
+                </div>
+            </div>
+            <div class="text-2xl font-semibold mt-2">Carro 1</div>
+        </div>
+    </div>
+</div>
