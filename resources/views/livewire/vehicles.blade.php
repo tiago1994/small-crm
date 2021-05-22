a33e4876b574be47ae80d8b8445d8bea0ae165ac
Tiago Matos <tiagovolta@gmail.com>
2021-05-22 11:45:58 -0300

    feat: populated vehicles blade

--- resources/views/livewire/vehicles.blade.php
+++ resources/views/livewire/vehicles.blade.php
@@ -1,16 +1,19 @@
 <div>
     <div class="grid grid-cols-3 gap-4">
-        <div class="col-span-1 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">    
-            <div class="flex">
-                <div class="flex-1">
-                    <button><i class="la la-play text-lg"></i></button>
+        @foreach ($vehicles as $vehicle)    
+            <div class="col-span-1 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">    
+                <div class="flex">
+                    <div class="flex-1">
+                        <button><i class="la la-play text-xl"></i></button>
+                        <button><i class="la la-pause text-xl"></i></button>
+                    </div>
+                    <div class="text-right flex-1">
+                        <button><i class="la la-edit text-xl"></i></button>
+                        <button><i class="la la-trash text-xl"></i></button>
+                    </div>
                 </div>
-                <div class="text-right flex-1">
-                    <button><i class="la la-edit text-lg"></i></button>
-                    <button><i class="la la-trash text-lg"></i></button>
-                </div>
+                <div class="text-2xl font-semibold mt-2">{{ $vehicle->name }}</div>
             </div>
-            <div class="text-2xl font-semibold mt-2">Carro 1</div>
-        </div>
+        @endforeach
     </div>
 </div>
