dcd53ca60fc394083ac8ff1aca48f648aefff047
Tiago Matos <tiagovolta@gmail.com>
2021-05-23 01:54:36 -0300

    feat: small changes on vehicles history

--- app/Http/Livewire/VehiclesHistory.php
+++ app/Http/Livewire/VehiclesHistory.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Vehicle;
 use Livewire\Component;
 
 class VehiclesHistory extends Component
 {
+    public $vehicle_id = '';
+
+    public function mount($id)
+    {
+        $this->vehicle_id = $id;
+    }
+
     public function render()
     {
-        return view('livewire.vehicles-history');
+        $vehicle = Vehicle::find($this->vehicle_id);
+        $histories = Vehicle::find($this->vehicle_id)->histories()->paginate(config('app.paginate_limit'));
+        return view('livewire.vehicles-history', ['histories' => $histories, 'vehicle' => $vehicle]);
     }
 }
